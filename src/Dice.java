c2ef3b753e8b6ccb3b900ea3b95586d5796dbad8
dniel <daniel.arias.castrillon@correounivalle.edu.co>
2023-05-13 21:53:40 -0500

    Created "Dice" class, pa los dados hermano

--- src/Dice.java
+++ src/Dice.java
@@ -1,2 +1,13 @@
+import java.util.Random;
+
 public class Dice {
+
+    private int face;
+    //Función que elige número random del 1 al 6 para los dados
+    public int getFace() {
+        Random rand = new Random();
+        face = rand.nextInt(6)+1;
+        return face;
+    }
+
 }
