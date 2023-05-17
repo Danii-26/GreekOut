cc620de87d9c37e0ab95d3541bb19989639b5376
dniel <daniel.arias.castrillon@correounivalle.edu.co>
2023-05-16 19:34:01 -0500

    Creada la clase del modelo de los dados

--- src/ModelCraps.java
+++ src/ModelCraps.java
@@ -1,2 +1,25 @@
 public class ModelCraps {
+
+    private Dice[] dado;
+    private int [] caras;
+
+    public ModelCraps(){
+        dado = new Dice[10];
+        for (int i=0; i<10;i++){
+            dado[i] =new Dice();
+        }
+        caras = new int[10];
+    }
+    public void obtenerValorCaras(){
+        for (int i=0; i<caras.length; i++){
+            caras[i]=dado[i].getFace(); // 1=Meeple, 2=42, 3=Héroe, 4=Dragón, 5=Corazón, 6=Cohete.
+        }
+    }
+    public int[] getCaras(){
+        return caras;
+    }
+    public static void main(String[] args) {
+        ModelCraps dado = new ModelCraps();
+        dado.getCaras();;
+    }
 }
